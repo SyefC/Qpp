81fbe36b50062cfe83c7a17b17ffa6ed25a2c5dc
SyefC <trisyefolionel@gmail.com>
2024-08-19 18:13:47 +0700

    q++.c 0.0.6 Update

--- Q++ File/q++.c
+++ Q++ File/q++.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main() {
   char arr[100];
+  char arrr[100];
 for(int a = 0; a < 100; a++) {
   printf("print:");
     fgets(arr,100,stdin);
@@ -22,6 +24,23 @@ for(int a = 0; a < 100; a++) {
 int c = a+b;
     printf("%d\n",c);
   }
+  }  
+  else if(strcmp(arr,"times()\n") == 0) {
+    for(int r = 0; r > -1; r++) {
+      int a; int b;
+      printf("ENTER FIRST NUMBER:");
+      scanf("%d", &a);
+      printf("ENTER SECOND NUMBER:");
+      scanf("%d",&b);
+      int c = a * b;
+      printf("%d\n", c);
+     if (a == 0 || b == 0) {
+       printf("END");
+       break;
+     }
+    }
   }
-}  
 }
+  return 0;
+}
+
